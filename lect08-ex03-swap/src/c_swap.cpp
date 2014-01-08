66da2d11c5b2d5c81263839b10999d93062c98d9
Remo Koch <rkoch@objeng.ch>
2014-01-08 14:35:45 +0100

    Add swap fns

---
+++ lect08-ex03-swap/src/c_swap.cpp
@@ -0,0 +1,12 @@
+/*
+ * c_swap.cpp
+ *
+ *  Created on: 08.01.2014
+ *      Author: rko
+ */
+
+void c_swap(int *a, int *b) {
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
---
+++ lect08-ex03-swap/src/swap.cpp
@@ -0,0 +1,12 @@
+/*
+ * swap.cpp
+ *
+ *  Created on: 08.01.2014
+ *      Author: rko
+ */
+
+void swap(int &a, int &b) {
+	int c = a;
+	a = b;
+	b = c;
+}
